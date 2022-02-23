2a1f38819a3e892f8b867e5f32d0f49bfda818f5
Eliboy <95360749+AxmadjonovEliboy@users.noreply.github.com>
2022-02-23 22:35:26 +0500

    four commit

--- src/main/java/uz/elmurodov/spring_boot/dto/task/TaskCreateDto.java
+++ src/main/java/uz/elmurodov/spring_boot/dto/task/TaskCreateDto.java
@@ -3,7 +3,8 @@ package uz.elmurodov.spring_boot.dto.task;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
-import uz.elmurodov.spring_boot.dto.Dto;
+import uz.elmurodov.spring_boot.dto.base.Dto;
+
 import java.time.LocalDateTime;
 
 /**
--- src/main/java/uz/elmurodov/spring_boot/reposiroty/project/ProjectRepository.java
+++ src/main/java/uz/elmurodov/spring_boot/reposiroty/project/ProjectRepository.java
@@ -2,11 +2,10 @@ package uz.elmurodov.spring_boot.reposiroty.project;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import uz.elmurodov.spring_boot.entity.project.Project;
-import uz.elmurodov.spring_boot.reposiroty.AbstractRepository;
 import uz.elmurodov.spring_boot.reposiroty.base.AbstractRepository;
 
 /**
  * @Author Aziza Tojiboyeva
  */
-public interface ProjectRepository extends AbstractRepository, JpaRepository<Project, Long>, AbstractRepository {
+public interface ProjectRepository extends JpaRepository<Project, Long>, AbstractRepository {
 }
